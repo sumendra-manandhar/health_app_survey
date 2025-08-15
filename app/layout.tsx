fb9ebcc844a87921bfd7b5329767a1c2300a72de
sumendra-manandhar <thesume.2010@gmail.com>
2025-08-16 00:32:12 +0545

    this

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,17 +15,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
+      <head>
+        <style>{`
+html {
+  font-family: ${GeistSans.style.fontFamily};
+  --font-sans: ${GeistSans.variable};
+  --font-mono: ${GeistMono.variable};
+}
+  
+        `}</style>
+      </head>
       <body>{children}</body>
     </html>
   );
 }
-
-//      <head>
-//         <style>{`
-// html {
-//   font-family: ${GeistSans.style.fontFamily};
-//   --font-sans: ${GeistSans.variable};
-//   --font-mono: ${GeistMono.variable};
-// }
-//         `}</style>
