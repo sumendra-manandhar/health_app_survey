c2e7d2f8cd40bf88b37a50226386cb1729602620
sumendra-manandhar <thesume.2010@gmail.com>
2025-08-16 02:20:45 +0545

    pus

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-    unoptimized: true, // for Next Image component on static sites
-  },
-  basePath: '/health_app_survey',
-  assetPrefix: '/health_app_survey/',
-
   eslint: {
     ignoreDuringBuilds: true,
   },
